b4027b15ad774a4f372785c6687ebb723d041997
YasinEnigma <amini.yasin1995@gmail.com>
2022-09-08 21:24:37 +0430

    Edit code for download and zip

--- task-001/microphone.py
+++ task-001/microphone.py
@@ -6,6 +6,34 @@ import queue
 import sounddevice as sd
 import vosk
 import sys
+import requests
+from tqdm import tqdm
+import zipfile
+from urllib.request import urlopen
+
+# download farsi models
+url = 'https://alphacephei.com/vosk/models/vosk-model-small-fa-0.5.zip'
+block_size = 1024 #1 Kibibyte
+
+filename = url.split("/")[-1]
+print(f"Downloading {filename}...")
+site = urlopen(url)
+meta = site.info()
+# Streaming, so we can iterate over the response.
+response = requests.get(url, stream=True)
+total_size_in_bytes = int(meta["Content-Length"])
+progress_bar = tqdm(total = total_size_in_bytes, unit='iB', unit_scale=True)
+with open('vosk-model-small-fa-0.5.zip', 'wb') as file:
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        file.write(data)
+progress_bar.close()
+print("Download complete")
+print(f"Extracting {filename}...")
+zip = zipfile.ZipFile(filename, "r")
+zip.extractall()
+zip.close()
+print("Extracting complete")
 
 q = queue.Queue()
 
