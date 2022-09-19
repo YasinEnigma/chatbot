89569a259c0c37c8753e4137c83e85d8037a7aa3
YasinEnigma <amini.yasin1995@gmail.com>
2022-09-20 01:26:27 +0430

    add task-004

---
+++ task-004/echo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import telebot
+
+with open('token', 'r') as token:
+	API_TOKEN = token.read()
+
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
